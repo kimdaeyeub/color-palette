eaa52dbb72ebc0ea9a6cc1fe449c4b85f48e55c5
kimdaeyeub <kdy3915@naver.com>
2024-02-25 23:34:14 +0900

    추가 페이지 완성

--- components/ColorForm.tsx
+++ components/ColorForm.tsx
@@ -1,21 +1,39 @@
-import React, { useState } from "react";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 
-const ColorForm = () => {
+interface IProp {
+  index: number;
+  setColors: Dispatch<SetStateAction<string[]>>;
+  setClear: Dispatch<SetStateAction<boolean>>;
+  colors: string[];
+  clear: boolean;
+}
+
+const ColorForm = ({ index, setColors, colors, clear, setClear }: IProp) => {
   const [colorCode, setColorCode] = useState("");
   const onChangeColorCode = (e: React.ChangeEvent<HTMLInputElement>) => {
     setColorCode(e.target.value);
+    let arr = colors;
+    arr[index] = e.target.value;
+    setColors(arr);
   };
+  useEffect(() => {
+    if (clear) {
+      setColorCode("");
+      setClear(false);
+    }
+  }, [clear]);
   return (
     <div
-      className={`w-full h-[300px] border border-black rounded-lg flex justify-center items-center p-5`}
+      className={`w-full aspect-video border border-black rounded-lg flex justify-center items-center p-5`}
       style={{ backgroundColor: colorCode }}
     >
       <input
         type="text"
         placeholder="Color code. ex)#3498db"
-        className="w-full px-4 py-2 rounded-md outline-none"
+        className="w-full px-4 py-2 outline-none bg-transparent border-b-2 border-black"
         value={colorCode}
         onChange={onChangeColorCode}
+        required
       />
     </div>
   );
--- components/Form.tsx
+++ components/Form.tsx
@@ -1,24 +1,61 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SelectBtn from "./SelectBtn";
 import ColorForm from "./ColorForm";
 
 const Form = () => {
-  const [gridValue, setGridValue] = useState<number>(9);
+  const [clear, setClear] = useState(false);
+  const [gridValue, setGridValue] = useState<number>(3);
+  const [colors, setColors] = useState<string[]>([]);
+  const [title, setTitle] = useState("");
+  const [description, setDiscription] = useState("");
   const onChangeSelectBtn = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    console.log(e.target.value);
+    const num = Number(e.target.value);
+    if (num === 9) {
+      setGridValue(3);
+      const newArr = new Array(9).fill("");
+      setColors(newArr);
+      setClear(true);
+    } else if (num === 16) {
+      setGridValue(4);
+      const newArr = new Array(16).fill("");
+      setColors(newArr);
+      setClear(true);
+    } else {
+      setGridValue(5);
+      const newArr = new Array(25).fill("");
+      setColors(newArr);
+      setClear(true);
+    }
+  };
+  const onChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+  const onChangeDescription = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDiscription(e.target.value);
   };
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(colors, title, description);
+  };
+  useEffect(() => {
+    const newArr = new Array(9).fill("");
+    setColors(newArr);
+  }, []);
+
+  console.log(clear);
   return (
     <div className="w-full h-full flex flex-col px-10 py-10 rounded-xl border-2 border-gray-400">
       {/* <SelectBtn /> */}
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="mt-4 flex flex-col justify-center items-start w-full">
           <label className="text-lg font-medium">title</label>
           <input
             type="text"
             required
+            onChange={onChangeTitle}
             placeholder="title"
             className="w-full outline-none bg-gray-200 border border-gray-400 px-4 py-2 rounded-lg mt-3"
           />
@@ -26,19 +63,30 @@ const Form = () => {
         <div className="mt-6 flex flex-col justify-center items-start w-full">
           <label className="text-lg font-medium">description</label>
           <textarea
+            onChange={onChangeDescription}
             className="rounded-xl resize-none bg-gray-200 border border-gray-400 w-full outline-none px-4 py-2 mt-3"
             rows={4}
           />
         </div>
         <div className="w-full mt-20 flex flex-col items-end justify-start">
           <SelectBtn onChangeSelectBtn={onChangeSelectBtn} />
-          <div className={`grid grid-cols-3 w-full mt-3 gap-4`}>
-            <ColorForm />
-            <ColorForm />
-            <ColorForm />
-            <ColorForm />
-            <ColorForm />
+          <div className={`grid grid-cols-${gridValue} w-full mt-3 gap-4`}>
+            {colors.map((color, index) => (
+              <ColorForm
+                key={index}
+                index={index}
+                clear={clear}
+                setClear={setClear}
+                setColors={setColors}
+                colors={colors}
+              />
+            ))}
           </div>
+          <input
+            value={"Submit"}
+            type="submit"
+            className="px-5 py-2 rounded-xl bg-black text-white text-lg font-medium mt-10"
+          />
         </div>
       </form>
     </div>
