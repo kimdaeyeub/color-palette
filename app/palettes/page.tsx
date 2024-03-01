ca7a09d13f235fbe277e89cfd5129d0adbf3c1c8
kimdaeyeub <kdy3915@naver.com>
2024-03-02 00:09:18 +0900

    palettes/ 페이지 서버컴포넌트로 변경 및 api수정

--- app/palettes/page.tsx
+++ app/palettes/page.tsx
@@ -1,45 +1,62 @@
-"use client";
 import ColorCard from "@/components/ColorCard";
 import { IPalette } from "@/utils/types";
-import { useSearchParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
 
-const page = () => {
-  const [palettes, setPalettes] = useState<IPalette[]>();
-  const params = useSearchParams();
-  useEffect(() => {
-    const mode = params.get("mode");
-    const fetchAllPalettes = async () => {
-      const response = await fetch("/api/palettes");
-      const data = await response.json();
-      setPalettes(data);
-    };
-    const fetchLightPalettes = async () => {
-      const response = await fetch("/api/palettes/light");
-      const json = await response.json();
-      setPalettes(json);
-    };
-    const fetchDarkPalettes = async () => {
-      const response = await fetch("/api/palettes/dark");
-      const json = await response.json();
-      setPalettes(json);
-    };
+const getDarkPalettes = async () => {
+  const response = await fetch("http://localhost:3000/api/palettes/dark");
+  const json = await response.json();
+  return json;
+};
+async function getLightPalettes() {
+  const response = await fetch("http://localhost:3000/api/palettes/lights");
+  const json = await response.json();
+  return json;
+}
 
-    if (mode === "dark") {
-      fetchDarkPalettes();
-    } else if (mode === "light") {
-      fetchLightPalettes();
-    } else {
-      fetchAllPalettes();
-    }
-  }, [params.get("mode")]);
-  return (
-    <div className="w-full h-screen px-32 py-10 grid grid-cols-3 gap-5">
-      {palettes?.map((palette, index) => (
-        <ColorCard key={index} palette={palette} />
-      ))}
-    </div>
-  );
+async function getAllPalettes() {
+  const response = await fetch("http://localhost:3000/api/palettes");
+  const json = await response.json();
+  return json;
+}
+
+const Palettes = async ({
+  searchParams: { mode },
+}: {
+  searchParams: { mode: string };
+}) => {
+  const [all, light, dark]: [
+    all: IPalette[],
+    light: IPalette[],
+    dark: IPalette[]
+  ] = await Promise.all([
+    getAllPalettes(),
+    getLightPalettes(),
+    getDarkPalettes(),
+  ]);
+  if (mode === undefined) {
+    return (
+      <div className="w-full h-screen px-32 py-10 grid grid-cols-3 gap-5">
+        {all?.map((palette, index) => (
+          <ColorCard key={index} palette={palette} />
+        ))}
+      </div>
+    );
+  } else if (mode === "light") {
+    return (
+      <div className="w-full h-screen px-32 py-10 grid grid-cols-3 gap-5">
+        {light?.map((palette, index) => (
+          <ColorCard key={index} palette={palette} />
+        ))}
+      </div>
+    );
+  } else if (mode === "dark") {
+    return (
+      <div className="w-full h-screen px-32 py-10 grid grid-cols-3 gap-5">
+        {dark?.map((palette, index) => (
+          <ColorCard key={index} palette={palette} />
+        ))}
+      </div>
+    );
+  }
 };
 
-export default page;
+export default Palettes;
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,4 @@
-"use client";
-import React, { useState } from "react";
-
 const Footer = () => {
-  console.log("Footer");
-  const [hello, setHello] = useState("");
   return <div>Footer</div>;
 };
 
