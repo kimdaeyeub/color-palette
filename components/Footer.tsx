1e8a56dea2f19f26eba09eb8eba3f7ba5024ffe6
kimdaeyeub <kdy3915@naver.com>
2024-03-12 17:41:53 +0900

    footer 업데이터

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,10 @@
 const Footer = () => {
   return (
-    <div className="w-full py-8 bg-[#222f3e] flex flex-col space-y-3 justify-center items-center">
-      <span className="text-lg text-white font-medium">kdy3915@naver.com</span>
-      <span className="text-lg text-white font-medium">김대엽</span>
+    <div className="w-full py-8 bg-[#FAFAFA] flex flex-col space-y-3 justify-center items-center">
+      <span className="text-lg text-gray-400 font-medium">
+        kdy3915@naver.com
+      </span>
+      <span className="text-lg text-gray-400 font-medium">김대엽</span>
     </div>
   );
 };
